41af7de19daf60b3c29cbc1b93332e03029122d3
glolorun <63762681+glolorun@users.noreply.github.com>
2020-07-11 23:02:31 -0400

    Production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,7 +10,6 @@ class ArticlesController < ApplicationController
   # GET /articles/1
   # GET /articles/1.json
   def show
-    article = Article.find(params[:id])
   end
 
   # GET /articles/new
@@ -20,12 +19,13 @@ class ArticlesController < ApplicationController
 
   # GET /articles/1/edit
   def edit
+
   end
 
   # POST /articles
   # POST /articles.json
   def create
-@article = Article.new(params.require(:article).permit(:title, :description))
+@article = Article.new(article_params)
 if @article.save
     flash[:notice] = "Article was creeated successfully."
 redirect_to article_path(@article)
@@ -37,25 +37,20 @@ end
   # PATCH/PUT /articles/1
   # PATCH/PUT /articles/1.json
   def update
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
-        format.json { render :show, status: :ok, location: @article }
-      else
-        format.html { render :edit }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
-    end
+if @article.update(article_params)
+    flash[:notice] = "Article was updated successfully."
+    redirect_to @article
+else
+    render 'edit'
   end
+end
 
   # DELETE /articles/1
   # DELETE /articles/1.json
   def destroy
     @article.destroy
-    respond_to do |format|
-      format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to articles_path
+
   end
 
   private
@@ -69,3 +64,4 @@ end
       params.require(:article).permit(:title, :description)
     end
 end
+
